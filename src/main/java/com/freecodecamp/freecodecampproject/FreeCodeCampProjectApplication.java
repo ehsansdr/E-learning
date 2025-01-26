acea2068fc4e3bd6917e57148afbdae401be88d4
ehsan <sehsan251@gmail.com>
2025-01-26 17:30:08 +0330

    first commit

---
+++ src/main/java/com/freecodecamp/freecodecampproject/FreeCodeCampProjectApplication.java
@@ -0,0 +1,13 @@
+package com.freecodecamp.freecodecampproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreeCodeCampProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FreeCodeCampProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/freecodecamp/freecodecampproject/FreeCodeCampProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.freecodecamp.freecodecampproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreeCodeCampProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
