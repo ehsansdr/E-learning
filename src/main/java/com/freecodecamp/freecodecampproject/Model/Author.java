22cc432c27da2700f69fa3765e07c624c753554f
ehsan <sehsan251@gmail.com>
2025-01-27 09:00:28 +0330

    author add table sequance

--- src/main/java/com/freecodecamp/freecodecampproject/Model/Author.java
+++ src/main/java/com/freecodecamp/freecodecampproject/Model/Author.java
@@ -9,13 +9,22 @@ public class Author {
 
     @Id
     @GeneratedValue(
-            strategy = GenerationType.SEQUENCE,
-            generator = "author_sequence"
+            strategy = GenerationType.TABLE,
+            generator = "author_id_generator"
     )
-    @SequenceGenerator(name = "author_sequence", // same as the value of the generatorin @GeneratedValue
+    // GenerationType.TABLE
+    /*@TableGenerator(name = "author_id_generator", // same as the generator in @GeneratedValue
+            table = "id_generator", // the table created by this name
+            pkColumnName = "id_name", // the column of the id_generator table
+            valueColumnName = "id_value", // the column of the id_generator table
+            allocationSize = 1
+    )*/
+
+    // GenerationType.SEQUENCE :
+    /* @SequenceGenerator(name = "author_sequence", // same as the value of the generatorin @GeneratedValue
             sequenceName = "author_sequence",
             allocationSize = 1 // how many size did it raise
-    )
+    )*/
     Integer id;
 
     String firstName;
