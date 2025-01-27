56bbb19c7de432fa97375bb127fe844cc91461de
ehsan <sehsan251@gmail.com>
2025-01-27 09:44:45 +0330

    create AuthorRepository

---
+++ src/main/java/com/freecodecamp/freecodecampproject/Repositories/AuthorRepository.java
@@ -0,0 +1,10 @@
+package com.freecodecamp.freecodecampproject.Repositories;
+
+import com.freecodecamp.freecodecampproject.Models.Author;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AuthorRepository extends JpaRepository<Author, Integer> {
+
+}
